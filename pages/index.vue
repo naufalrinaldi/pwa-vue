ad1e01b6c3aa04330202b6a0c90bfc9dbf7571c4
naufalrinaldi <60633306+naufalrinaldi@users.noreply.github.com>
2020-04-23 18:25:23 +0700

    Fix

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+<div id="app">
+      <Navbar />
+      <Card/>
+</div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+import Card from '~/components/Card.vue'
+
+export default {
+  name : 'navbar',
+  components: {
+    Navbar,
+    Card
+    
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
